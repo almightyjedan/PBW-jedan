27d079b5db0d404d43ec4d3f378ff656c33500f6
almightyjedan <zaidan.xoxo@gmail.com>
2025-10-03 10:55:05 +0700

    Instalasi, Konfigurasi, dan Halaman Pertama Kita

---
+++ PRAK 2 - Instalasi, Konfigurasi, dan Halaman Pertama Kita/LaraPress/resources/views/kontak.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Kami - LaraPress</title>
+</head>
+<body>
+    <h1>Kontak LaraPress</h1>
+    <p>Nama : Muhammad Zaidan Ahbab</p>
+    <p>NPM : 4523210081</p>
+    <p>Email : jedan4523081@univpancasila.ac.id</p>
+    <p>No.Hp : 085813334673</p>
+    <a href="/">Kembali ke Halaman Utama</a>
+    <a href="/tentang-kami">Lihat Halaman Tentang Kami</a>
+</body>
+</html>
